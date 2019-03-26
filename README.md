6314dff6cf64cc80295a1b8a6eec7207e86b2621
ajsevak <alina967@live.com>
2019-03-26 10:39:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ykaisb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ykaisb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+export class AppComponent  {
+  name = 'Angular';
+   foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+}
+ 
